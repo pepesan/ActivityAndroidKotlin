89e6e27dc76b819203c29d19f19dea41198630d2
David <dvaquero@bilbomatica.es>
2021-02-25 16:23:27 +0100

    Intent para hacer llamadas

--- app/src/main/java/com/cursosdedesarrollo/activityandroidkotlin/SegundaActivity.kt
+++ app/src/main/java/com/cursosdedesarrollo/activityandroidkotlin/SegundaActivity.kt
@@ -1,18 +1,26 @@
 package com.cursosdedesarrollo.activityandroidkotlin
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 
 
 class SegundaActivity : AppCompatActivity() {
+    var buttonLlamada : Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +47,57 @@ class SegundaActivity : AppCompatActivity() {
         }catch (e:Exception){
             println("por si acaso")
         }
+        buttonLlamada= findViewById(R.id.button_llamada)
+        buttonLlamada?.setOnClickListener{
+            Log.d("app","Intenta llamada")
+            intentaLlamada()
+        }
+    }
+    fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.CALL_PHONE)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.CALL_PHONE),
+                        42)
+            }
+        } else {
+            // Permission has already been granted
+            callPhone()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == 42) {
+            // If request is cancelled, the result arrays are empty.
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                // permission was granted, yay!
+                callPhone()
+            } else {
+                // permission denied, boo! Disable the
+                // functionality
+            }
+            return
+        }
+    }
+
+    fun callPhone(){
+        val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "+34 656 66 14 78"))
+        startActivity(intent)
+    }
+    private fun intentaLlamada() {
+        checkPermission()
     }
 
     fun volver(view: View):Unit{
